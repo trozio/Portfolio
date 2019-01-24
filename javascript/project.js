734da69fb0d372b797266e8f6bae709c93d06309
trozio <daniel.l.adelman@gmail.com>
2019-01-24 18:40:37 -0500

    js and responsiveness

--- javascript/project.js
+++ javascript/project.js
@@ -2,3 +2,12 @@ M.AutoInit();
 $(document).ready(function(){
     $('.sidenav').sidenav();
   });
+
+$("#submit").on("click", function(event){
+	event.preventDefault();
+	let name = $("#first_name").val();
+	let email = $("#email").val();
+	let message = $("#message").val();
+
+	
+})
