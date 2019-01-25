cce7e8b126764a3355f1dcba8df362ad31cb8ae8
trozio <daniel.l.adelman@gmail.com>
2019-01-25 13:43:51 -0500

    css

--- javascript/project.js
+++ javascript/project.js
@@ -8,6 +8,18 @@ $("#submit").on("click", function(event){
 	let name = $("#first_name").val();
 	let email = $("#email").val();
 	let message = $("#message").val();
-
-	
+    // sendMail();
+    $("#first_name").empty();
+    $("#email").empty();
+    $("#message").empty();
+	M.textareaAutoResize($("#message"));
 })
+// function sendMail() {
+// var link = "mailto:daniel.l.adelman@gmail.com"
+//          + "?cc=myCCaddress@example.com"
+//          + "&subject=" + escape("This is my subject")
+//          + "&body=" + escape($("#first_name").val() + $("#email").val() + $("#message").val())
+// ;
+//
+// window.location.href = link;
+// }
