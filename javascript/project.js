a51c058c4e3c339426376c2951079da254f21ea5
trozio <daniel.l.adelman@gmail.com>
2019-01-26 13:44:42 -0500

    Update project.js

--- javascript/project.js
+++ javascript/project.js
@@ -11,7 +11,7 @@ $(document).ready(function(){
 
   setTimeout(function(){
       $("#icon").addClass("scale-in");
-  }, 500)
+  }, 1000)
 
 $("#submit").on("click", function(event){
 	event.preventDefault();
