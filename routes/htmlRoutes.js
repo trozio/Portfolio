561501fa44aa477e2f5f4db65097fc1376a63b64
Daniel Adelman <daniel.l.adelman@gmail.com>
2020-08-13 01:24:18 -0400

    fixed img bugs

--- routes/htmlRoutes.js
+++ routes/htmlRoutes.js
@@ -4,7 +4,6 @@ let express = require("express");
 
 module.exports = function(app) {
 	app.get("/", function(req, res) {
-        db.Posts.findAll();
 			res.sendFile(path.join(__dirname, "../index.html"));
 })
 }
--- server.js
+++ server.js
@@ -6,11 +6,10 @@ var PORT = process.env.PORT || 3000;
 
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
-app.use(express.static("public"));
-
+app.use(express.static(__dirname + '/public'));
 
 require("./routes/htmlroutes.js")(app);
-require("./routes/apiroutes.js")(app);
+// require("./routes/apiroutes.js")(app);
 
 
 app.listen(PORT, function() {
