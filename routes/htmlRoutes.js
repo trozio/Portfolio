6a1198fa10d792548eaf1dc07b91e22143cded89
Daniel Adelman <daniel.l.adelman@gmail.com>
2020-08-13 19:13:38 -0400

    removed routes folder

--- routes/htmlRoutes.js
+++
@@ -1,9 +0,0 @@
-let path = require("path");
-let express = require("express");
-
-
-module.exports = function(app) {
-	app.get("/", function(req, res) {
-			res.sendFile(path.join(__dirname, "../index.html"));
-})
-}
--- server.js
+++ server.js
@@ -1,16 +1,20 @@
 let express = require("express");
 let app = express();
+let path = require("path");
 const http = require('http').Server(app);
 
-var PORT = process.env.PORT || 3000;
+var PORT = 3000;
 
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 app.use(express.static(__dirname + '/public'));
 
-require("./routes/htmlRoutes.js")(app);
+// require("./routes/htmlRoutes.js")(app);
 // require("./routes/apiroutes.js")(app);
 
+app.get("/", function(req, res) {
+    res.sendFile(path.join(__dirname, "index.html"));
+});
 
 app.listen(PORT, function() {
   console.log("App listening on PORT " + PORT);
