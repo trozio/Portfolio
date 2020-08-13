fbd15357d8d552ea3967998ec77c344ac9e7223e
Daniel Adelman <daniel.l.adelman@gmail.com>
2020-08-13 19:34:23 -0400

    Update server.js

--- server.js
+++ server.js
@@ -1,6 +1,7 @@
 let express = require("express");
 let app = express();
 let path = require("path");
+const hostname = "localhost";
 const http = require('http').Server(app);
 
 var PORT = 3000;
@@ -14,6 +15,12 @@ app.use(express.static(__dirname + '/public'));
 
 app.get("/", function(req, res) {
     res.sendFile(path.join(__dirname, "index.html"));
+});
+
+const server = http.createServer((req, res) => {
+  res.statusCode = 200;
+  res.setHeader('Content-Type', 'text/html');
+  res.end(helpText);
 });
 
 app.listen(PORT, function() {
