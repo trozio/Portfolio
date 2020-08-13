d86561da39ed38317c6dbd28a60cdefa88e262ad
Daniel Adelman <daniel.l.adelman@gmail.com>
2020-08-13 15:39:57 -0400

    Update server.js

--- server.js
+++ server.js
@@ -2,7 +2,7 @@ let express = require("express");
 let app = express();
 const http = require('http').Server(app);
 
-var PORT = process.env.PORT || 3000;
+var PORT = process.env.PORT || 8000;
 
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
