130c880b8facb0b019d479d343450a888a53e1da
Daniel Adelman <daniel.l.adelman@gmail.com>
2020-08-18 19:03:49 -0400

    mail

--- public/javascript/project.js
+++ public/javascript/project.js
@@ -1,5 +1,5 @@
 M.AutoInit();
-// const nodemailer = require('nodemailer');
+let mailFunc = require("../../mail.js");
 
 $(document).ready(function(){
     $('.parallax').parallax();
@@ -25,30 +25,9 @@ $("#submit").on("click", function(event){
   mail.email = email;
   mail.message = message;
 
+  mailFunc.sendMail(mail);
 
 
 
-  const transporter = nodemailer.createTransport({
-    service: 'outlook.office.com',
-    auth: {
-      user: 'daniel@daniadelman.com',
-      pass: 'pr1ngad2!'
-    }
-  });
-
-  let mailOptions = {
-    from: 'daniel@daniadelman.com',
-    to: 'daniel@daniadelman.com',
-    subject: 'New Client',
-    text: mail
-  };
-
-  transporter.sendMail(mailOptions, function(error, info){
-    if (error) {
-      console.log(error);
-    } else {
-      console.log('Email sent: ' + info.response);
-    }
-  });
-  	M.textareaAutoResize($("#message"));
+  M.textareaAutoResize($("#message"));
 })
